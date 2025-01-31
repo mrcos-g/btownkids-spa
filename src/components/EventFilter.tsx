7812370fc026f6c16bb8b7c8f0f95ddbe34d685f
mrcos-g <marcgonzalez@outlook.com>
2025-01-31 13:29:57 -0500

    modify grid cols for sources to keep inline on laptop and larger

--- src/components/EventFilter.tsx
+++ src/components/EventFilter.tsx
@@ -30,7 +30,7 @@ const EventFilter: FC<EventFilterProps> = ({ onFilterChange }) => {
         <div>
           <h3 className="text-lg md:text-xl">Filter by Source</h3>
         </div>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
           {sources.map((source) => {
             return (
               <label key={source} className="mx-2 flex items-center space-x-4">
