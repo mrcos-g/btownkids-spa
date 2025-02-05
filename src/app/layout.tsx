9b0f72516d94b15d5bc0dff388b2b2c94b35d1f8
mrcos-g <marcgonzalez@outlook.com>
2025-02-04 21:52:10 -0500

    create event source context to move state globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { EventSourceProvider } from '@/context/EventSourceContext';
 import Navigation from '@/components/Navigation';
 
 const geistSans = Geist({
@@ -33,8 +34,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Navigation />
-        {children}
+        <EventSourceProvider>
+          <Navigation />
+          {children}
+        </EventSourceProvider>
       </body>
     </html>
   );
---
+++ src/context/EventSourceContext.tsx
@@ -0,0 +1,48 @@
+'use client';
+import { createContext, useContext, useState, ReactNode } from 'react';
+
+export enum EventSource {
+  ELLETTSVILLE = 'Ellettsville Branch (MCPL)',
+  SOUTHWEST = 'Southwest Branch (MCPL)',
+  DOWNTOWN = 'Downtown Library (MCPL)',
+  VISIT_BLOOMINGTON = 'VisitBloomington',
+}
+
+interface EventSourceContextType {
+  selectedSources: string[];
+  setSelectedSources: (source: string[]) => void;
+}
+
+interface EventSourceProviderProps {
+  children: ReactNode;
+}
+
+const EventSourceContext = createContext<EventSourceContextType | undefined>(undefined);
+
+export const EventSourceProvider = ({ children }: EventSourceProviderProps) => {
+  const [selectedSources, setSelectedSources] = useState<string[]>(Object.values(EventSource));
+
+  const setStateSelectedSources = (source: string) => {
+    setSelectedSources((previousSelectedSources) =>
+      previousSelectedSources.includes(source)
+        ? previousSelectedSources.filter((selectedSource) => selectedSource !== source)
+        : [...previousSelectedSources, source],
+    );
+  };
+
+  return (
+    <EventSourceContext.Provider value={{ selectedSources, setStateSelectedSources }}>
+      {children}
+    </EventSourceContext.Provider>
+  );
+};
+
+export const useEventSourceContext = () => {
+  const context = useContext(EventSourceContext);
+
+  if (!context) {
+    throw new Error('useEventSourceContext must be used within an EventSourceProvider');
+  }
+
+  return context;
+};
