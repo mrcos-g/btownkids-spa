3d6ea427651933eebc8018be36f4d7fdbe77745a
mrcos-g <marcgonzalez@outlook.com>
2025-02-13 11:04:00 -0500

    set initial handle event set for moving forwards and backwards in months

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Box } from '@mui/material';
 import FullCalendar from '@fullcalendar/react';
 import dayGridView from '@fullcalendar/daygrid';
@@ -9,20 +9,36 @@ import { FormattedVisitBloomEvent } from '@/app/calendar/page';
 import '../app/globals.css';
 
 interface CalendarProps {
-  events: FormattedVisitBloomEvent[] | null;
+  initialEvents?: FormattedVisitBloomEvent[];
   error?: string | null;
 }
 
-const Calendar: FC<CalendarProps> = ({ events, error }) => {
+const Calendar: FC<CalendarProps> = ({ initialEvents = [], error }) => {
   const { selectedSources } = useEventSourceContext();
+
+  const [events, setEvents] = useState<FormattedVisitBloomEvent[]>(
+    initialEvents.filter((event) => selectedSources.includes(event.source)),
+  );
+
+  useEffect(() => {
+    setEvents(initialEvents.filter((event) => selectedSources.includes(event.source)));
+  }, [initialEvents, selectedSources]);
+
+  const handleEventSet = async (dateInfo: { startStr: string; endStr: string }) => {
+    const { startStr, endStr } = dateInfo;
+    console.log({ startStr, endStr });
+
+    const apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/all-events`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+
+    // setEvents(data);
+  };
 
   if (error) {
     return <div>Failed to fetch events: </div>;
   }
-
-  const filteredEvents = (events || []).filter((event) => {
-    return selectedSources.includes(event.source);
-  });
 
   return (
     <Box sx={{ pt: 8 }}>
@@ -30,7 +46,8 @@ const Calendar: FC<CalendarProps> = ({ events, error }) => {
         <FullCalendar
           plugins={[dayGridView, listMonth]}
           initialView="listMonth"
-          events={filteredEvents}
+          events={events}
+          datesSet={handleEventSet}
           headerToolbar={{
             left: 'prev,next today',
             center: 'title',
